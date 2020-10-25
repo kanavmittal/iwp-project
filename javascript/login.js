286bc4b6ec6f3bb222604efa676eb4162a89ec93
kanavmittal <kanavmittal0@mail.com>
2020-10-25 11:39:38 +0530

    final login page

--- javascript/login.js
+++ javascript/login.js
@@ -7,3 +7,16 @@ function Validate() {
     }
     return true;
 }
+
+document.getElementById('getval').addEventListener('change', readURL, true);
+function readURL(){
+    var file = document.getElementById("getval").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function(){
+        document.getElementById('profile-upload').style.backgroundImage = "url(" + reader.result + ")";        
+    }
+    if(file){
+        reader.readAsDataURL(file);
+    }else{
+    }
+}
